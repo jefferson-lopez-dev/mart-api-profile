eaf2558979f8f2f90e4fd31efd36acafaffd92ba
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-27 17:31:44 -0500

    delete files

--- src/middlewares/validated.js
+++
@@ -1,8 +0,0 @@
-export const validateSchema = (schema) => (req, res, next) => {
-  try {
-    schema.parse(req.body);
-    next();
-  } catch (error) {
-    return res.json({ message: error.issues.map((err) => err.message) });
-  }
-};
