cc83c7aeb9f116b29703cf18d5e61f773c5e464e
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-29 16:21:10 -0500

    configure mongo

--- src/data-base.js
+++ src/data-base.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-const URL_DATABASE = process.env.URL_MONGO_DB_LOCAL;
+const URL_DATABASE = process.env.URL_MONGO_DB_ATLAS;
 
 export const ConnectionDB = async () => {
   try {
-    await mongoose.connect(URL_DATABASE);
-    console.log("DB is connected");
+    await connect(URL_DATABASE);
+    console.log("● DB is connected");
   } catch (error) {
-    console.log("Disconnected to db", error);
+    console.log("● Disconnected to db", error);
   }
 };
