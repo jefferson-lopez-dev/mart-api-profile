1759cc01cbff419fc57a843198d0cbf71ef29499
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-12-02 21:24:06 -0500

    rediret url to atlas

--- src/data-base.js
+++ src/data-base.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-const URL_DATABASE = process.env.URL_MONGO_DB_LOCAL;
+const URL_DATABASE = process.env.URL_MONGO_DB_ATLAS;
 
 export const ConnectionDB = async () => {
   try {
